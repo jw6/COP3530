abd0e8c515971ab0031a97305ecbccee80eb32ae
Jay Wang <jaywang007@yahoo.com>
2016-01-28 08:49:26 -0500

    a

--- practice/practice/main.cpp
+++ practice/practice/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+using namespace std;
 
+void functionC(int a[], int size, int target){
+    int i,j;
+    i = 0;
+    j = size - 1;
+    
+    while(i < j){
+        if(a[i] + a[j] == target){
+            cout << a[i] << " " << a[j];
+            break;
+        }
+    
+        else if (a[i] + a[j] > target)
+            j--;
+        else
+            i++;
+    }
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int a[] = {1,5,6,7,8,9};
+    int b = sizeof(a)/ sizeof(int);
+    int c = 9;
+
+    functionC(a,b,c);
+
+    
+    return EXIT_SUCCESS;
 }
--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,15 +1,99 @@
-//
-//  main.cpp
-//  test
-//
-//  Created by Jay on 1/27/16.
-//  Copyright (c) 2016 Jay. All rights reserved.
-//
-
 #include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+typedef int dataType;
+
+struct node{
+    dataType data;
+    node *next;
+};
+
+class linker{
+public:
+    linker();	//constructor
+    ~linker();	//destructor
+    
+    void create(int &x); //create a linker
+    // bool insert();
+    //insert data
+    void print();	//print linker
+    
+private:
+    node *head;	//linker head pointer
+};
+
+linker::linker(){
+    head = NULL;
+}
+
+linker::~linker(){
+    node *ptrNode = head;
+    node *ptrNodeTemp = NULL;
+    
+    while(ptrNode != NULL){
+        ptrNodeTemp = ptrNode->next;
+        delete ptrNode;
+        ptrNode =ptrNodeTemp;
+    }
+}
+
+void linker::create(int &numOfElement){
+    node *ptrNode = new node;
+    ptrNode->data = 0;
+    head = ptrNode;
+    
+    
+    bool flagNum = true;
+    dataType data = 0;
+    
+    // for(int x = numOfElement; x >= 0; x--){
+    while(numOfElement >= 0){
+        
+        if(numOfElement == 0){
+            ptrNode->next = NULL;
+            --numOfElement;
+        }
+        else{
+            cin >> data;
+            
+            ptrNode->data = data;
+            flagNum = false;
+            
+            if(!flagNum){ //keep create slot
+                node *temp = new node;
+                ptrNode->next = temp;
+                ptrNode = temp;
+            }
+            
+            --numOfElement;
+        }
+        
+    }
+    
+    if(head->data == 0){
+        head = NULL;
+        delete ptrNode;
+    }
+}
+
+void linker::print(){
+    node *ptrNode = head;
+    
+    while(ptrNode != NULL){
+        cout << ptrNode->data << " ";
+        ptrNode = ptrNode->next;
+    }
+    cout << endl;
+}
+
+int main(int argc, char const* argv[]){
+    linker x;
+    int numOfElement;
+    x.create(numOfElement);
+    
+    x.print();
+    
+ 
 }
